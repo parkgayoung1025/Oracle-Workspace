2eeb904fc377928e435a025aeeec7f9d8cfb2517
th816 <117423689+th816@users.noreply.github.com>
2022-12-20 13:53:07 +0900

    SELECT_SUBQUERY

--- 05_DML(SELECT)_SUBQUERY.sql
+++ 05_DML(SELECT)_SUBQUERY.sql
@@ -222,3 +222,160 @@ WHERE (JOB_CODE, MANAGER_ID) = (SELECT JOB_CODE, MANAGER_ID
     
     서브쿼리 조회 결과가 여러 행, 여러 칼럼일 경우
 */
+
+-- 각 직급별 최소 급여를 받는 사원들 조회(사번, 이름, 직급 코드, 급여)
+-- 1) 각 직급별 최소 급여를 조회
+SELECT JOB_CODE, MIN(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE;
+
+-- 2) 각 직급별 최소 급여와 일치하는 사원 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE (JOB_CODE = 'J1' AND SALARY = '8000000') 
+   OR (JOB_CODE = 'J2' AND SALARY = '3700000')
+   OR (JOB_CODE = 'J3' AND SALARY = '3400000')
+   OR (JOB_CODE = 'J4' AND SALARY = '1550000')
+   OR (JOB_CODE = 'J5' AND SALARY = '2200000')
+   OR (JOB_CODE = 'J6' AND SALARY = '2000000')
+   OR (JOB_CODE = 'J7' AND SALARY = '1380000');
+   
+-- 3) 위 내용을 가지고 하나의 쿼리문으로 만들기
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE (JOB_CODE, SALARY) IN (SELECT JOB_CODE, MIN(SALARY)
+                             FROM EMPLOYEE
+                             GROUP BY JOB_CODE); 
+
+-- 각 부서별로 최고 급여를 받는 사원들 조회(사번, 이름, 부서 코드, 급여)
+SELECT NVL(DEPT_CODE, '없음'), MAX(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+-- 부서가 없는 사원의 경우 없음이라는 부서로 출력되도록
+SELECT EMP_ID, EMP_NAME, NVL(DEPT_CODE, '없음'), SALARY
+FROM EMPLOYEE
+WHERE (NVL(DEPT_CODE, '없음'), SALARY) IN (SELECT NVL(DEPT_CODE, '없음'), MAX(SALARY)
+                              FROM EMPLOYEE
+                              GROUP BY DEPT_CODE)
+ORDER BY SALARY DESC;
+
+/*
+    5. 인라인 뷰(INLINE VIEW)
+    FROM 절에 서브쿼리를 제시하는 것
+    
+    서브쿼리를 수행한 결과를 테이블 대신 사용하는 개념
+*/
+
+-- 보너스 포함 연봉이 3000만원 이상인 사원들의 사번, 이름, 보너스 포함 연봉, 부서 코드 조회
+SELECT EMP_ID, EMP_NAME, (SALARY + SALARY * NVL(BONUS, 0)) *12 "보너스 포함 연봉", DEPT_CODE
+FROM EMPLOYEE
+WHERE (SALARY + SALARY * NVL(BONUS, 0)) *12 >= 30000000;
+
+-- 인라인 뷰 사용 : 사원명만 골라내기 (보너스 포함 연봉이 3000만원 이상인 사원들의 이름만)
+SELECT EMP_NAME
+FROM (SELECT EMP_ID, EMP_NAME, (SALARY + SALARY * NVL(BONUS, 0)) *12 "보너스 포함 연봉", DEPT_CODE
+      FROM EMPLOYEE
+      WHERE (SALARY + SALARY * NVL(BONUS, 0)) *12 >= 30000000)
+WHERE DEPT_CODE IS NULL;
+
+-- 인라인 뷰를 주로 사용하는 예
+-- TOP - N 분석 : 데이터 베이스상에 있는 자료 중 최상위 N개의 자료를 보기 위해 사용하는 기능
+
+-- 전 직원 중 급여가 가장 높은 상위 5명(순위, 사원명, 급여)
+-- *ROWNUM : 오라클에서 자동으로 제공해 주는 칼럼. "조회된" 순서대로 1부터 순번을 부여해 주는 칼럼
+
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM EMPLOYEE
+ORDER BY SALARY DESC;
+-- 에러 원인 : ORDER BY로 정렬하기 전에 이미 ROWNUM의 순번이 매겨져 있기 때문
+-- 해결 방법 : ORDER BY로 이미 정렬할 테이블을 가지고 ROWNUM 순번을 매기면 됨
+
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM (SELECT *
+      FROM EMPLOYEE
+      ORDER BY SALARY DESC)
+WHERE ROWNUM <= 5;
+
+-- 각 부서별 평균 급여가 높은 3개의 부서 코드, 평균 급여 조회
+-- 1) 각 부서별 평균 급여 => 높은 순서대로 정렬
+SELECT DEPT_CODE, ROUND(AVG(SALARY))
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+ORDER BY 2 DESC;
+
+-- 2) 순번 부여, 가장 많이 받는 3개 부서만 추리기
+SELECT ROWNUM, DEPT_CODE, SALARY
+FROM (SELECT DEPT_CODE, ROUND(AVG(SALARY)) SALARY
+      FROM EMPLOYEE
+      GROUP BY DEPT_CODE
+      ORDER BY 2 DESC)
+WHERE ROWNUM <= 3;
+
+SELECT ROWNUM, S.*
+FROM (SELECT DEPT_CODE, ROUND(AVG(SALARY))
+      FROM EMPLOYEE
+      GROUP BY DEPT_CODE
+      ORDER BY 2 DESC) S
+WHERE ROWNUM <= 3;
+
+SELECT ROWNUM, DEPT_CODE, "ROUND(AVG(SALARY))"
+FROM (SELECT DEPT_CODE, ROUND(AVG(SALARY))
+      FROM EMPLOYEE
+      GROUP BY DEPT_CODE
+      ORDER BY 2 DESC)
+WHERE ROWNUM <= 3;
+
+-- ROWNUM을 이용하면 순위를 매길 수 있음
+-- 다만, 정렬이 되지 않은 상태에서는 순위를 매기면 의미가 없으므로
+-- 선 정렬 후 순위 매기기를 해야 한다 => 우선적으로 인라인 뷰로 ORDER BY 절을 만들고 그 후에 메인 쿼리에서 순번을 붙임
+
+-- 가장 최근에 입사한 사원 5명(사원명, 급여, 입사일)
+SELECT EMP_NAME, SALARY, HIRE_DATE
+FROM EMPLOYEE
+ORDER BY HIRE_DATE DESC;
+
+SELECT ROWNUM, EMP_NAME, SALARY, HIRE_DATE
+FROM (SELECT EMP_NAME, SALARY, HIRE_DATE
+      FROM EMPLOYEE
+      ORDER BY HIRE_DATE DESC)
+WHERE ROWNUM <= 5;
+
+/*
+    6. 순위 매기는 함수(WINDOW FUNCTION)
+    RANK() OVER(정렬 기준)
+    DENSE_RANK() OVER(정렬 기준)
+    
+    - RANK() OVER(정렬 기준) : 공동 1위가 3명이라고 한다면 그다음 순위는 4위로
+    - DENSE_RANK() OVER(정렬기 준) : 공동 1위가 3명이면 그다음 순위는 2위로
+    
+    정렬 기준 : ORDER BY 절(정렬 기준 칼럼 이름, 오름차순/내림차순), NULLFIRST나 NULLSLAST는 기술 불가
+    
+    오직 SELECT 절에서만 기술 가능
+*/
+
+-- 사원들의 급여가 높은 순서대로 매기기. 사원명, 급여, 순위 조회 : RANK OVER
+SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) 순위
+FROM EMPLOYEE; -- 공동 19위 2명, 그 다음 순위는 21위
+
+SELECT EMP_NAME, SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) 순위
+FROM EMPLOYEE; -- 공동 19위 2명, 그 다음 순위는 20위
+
+-- 5위까지만 조회
+SELECT EMP_NAME, SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) 순위
+FROM EMPLOYEE
+WHERE DENSE_RANK() OVER(ORDER BY SALARY DESC) <= 5;
+-- 윈도우 함수를 WHERE 절에서 기술 불가능
+
+-- 인라인 뷰로 변경
+-- 1) RANK 함수로 순위를 매기고 정렬까지 완료
+SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) 순위
+FROM EMPLOYEE;
+
+-- 인라인 뷰로 전환
+SELECT *
+FROM (
+    SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) 순위
+    FROM EMPLOYEE
+    )
+WHERE 순위 <= 5;
